37c9530594aaeb470a8f7c3eb9b32a2a19cf31b0
wangdongdong <wangdd_bj@163.com>
2017-06-29 14:23:26 +0800

    Initial commit

---
+++ src/main/java/com/mazouri/grpcLib_android_sample/HelloServer.java
@@ -0,0 +1,122 @@
+package com.mazouri.grpcLib_android_sample;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+import io.grpc.examples.helloworld.GreeterGrpc;
+import io.grpc.examples.helloworld.HelloRequest;
+import io.grpc.examples.helloworld.HelloResponse;
+
+/**
+ * Created by wangdongdong on 17/6/29.
+ */
+
+public class HelloServer {
+
+    private int port = 8080;
+    private boolean useTls = false;
+
+    private ScheduledExecutorService executor;
+    private Server server;
+
+    public static void main(String[] args ) throws Exception {
+        final HelloServer server = new HelloServer();
+        server.parseArgs(args);
+        if (server.useTls) {
+            System.out.println(
+                    "\nUsing fake CA for TLS certificate. Test clients should expect host\n"
+                            + "*.test.google.fr and our test CA. For the Java test client binary, use:\n"
+                            + "--server_host_override=foo.test.google.fr --use_test_ca=true\n");
+        }
+
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                try {
+                    System.out.println("Shutting down");
+                    server.stop();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        server.start();
+        System.out.println("HelloServer started on port " + server.port);
+    }
+
+    private void parseArgs(String[] args) {
+        boolean usage = false;
+        for (String arg : args) {
+            if (!arg.startsWith("--")) {
+                System.err.println("All arguments must start with '--': " + arg);
+                usage = true;
+                break;
+            }
+            String[] parts = arg.substring(2).split("=", 2);
+            String key = parts[0];
+            if ("help".equals(key)) {
+                usage = true;
+                break;
+            }
+            if (parts.length != 2) {
+                System.err.println("All arguments must be of the form --arg=value");
+                usage = true;
+                break;
+            }
+            String value = parts[1];
+            if ("port".equals(key)) {
+                port = Integer.parseInt(value);
+            } else if ("use_tls".equals(key)) {
+                useTls = Boolean.parseBoolean(value);
+            } else if ("grpc_version".equals(key)) {
+                if (!"2".equals(value)) {
+                    System.err.println("Only grpc version 2 is supported");
+                    usage = true;
+                    break;
+                }
+            } else {
+                System.err.println("Unknown argument: " + key);
+                usage = true;
+                break;
+            }
+        }
+        if (usage) {
+            HelloServer s = new HelloServer();
+            System.out.println(
+                    "Usage: [ARGS...]"
+                            + "\n"
+                            + "\n  --port=PORT           Port to connect to. Default " + s.port
+                            + "\n  --use_tls=true|false  Whether to use TLS. Default " + s.useTls
+            );
+            System.exit(1);
+        }
+    }
+
+    private void start() throws Exception {
+        executor = Executors.newSingleThreadScheduledExecutor();
+        server = ServerBuilder.forPort(port)
+                .addService(new GreeterImpl())
+                .build().start();
+    }
+
+    private void stop() throws Exception {
+        if (server != null) {
+            server.shutdown();
+        }
+    }
+
+    static class GreeterImpl extends GreeterGrpc.GreeterImplBase {
+
+        @Override
+        public void sayHello(HelloRequest req, StreamObserver<HelloResponse> responseObserver) {
+            HelloResponse reply = HelloResponse.newBuilder().setMessage("Hello " + req.getName()).build();
+            responseObserver.onNext(reply);
+            responseObserver.onCompleted();
+        }
+
+
+    }
+}
